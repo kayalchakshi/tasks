5ada2ede8900dc90fa29702a65f56b1f1becbab3
kayalchakshi <35140705+kayalchakshi@users.noreply.github.com>
2018-10-29 13:52:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imagecomp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Program obj = new Program();
+            obj.CompressImage(Image.FromFile("C:/Users/IS7063/Desktop/tasks/pic.jpg"),
+                             30, "C:/Users/IS7063/Desktop/tasks/compressedPicture.jpg");
+        }
+        private void CompressImage(Image sourceImage, int imageQuality, string savePath)
+        {
+            try
+            {
+                //Create an ImageCodecInfo-object for the codec information
+                ImageCodecInfo jpegCodec = null;
+
+                //Set quality factor for compression
+                EncoderParameter imageQualitysParameter = new EncoderParameter(
+                            System.Drawing.Imaging.Encoder.Quality, imageQuality);
+
+                //List all avaible codecs (system wide)
+                ImageCodecInfo[] alleCodecs = ImageCodecInfo.GetImageEncoders();
+
+                EncoderParameters codecParameter = new EncoderParameters(1);
+                codecParameter.Param[0] = imageQualitysParameter;
+
+                //Find and choose JPEG codec
+                for (int i = 0; i < alleCodecs.Length; i++)
+                {
+                    if (alleCodecs[i].MimeType == "image/jpeg")
+                    {
+                        jpegCodec = alleCodecs[i];
+                        break;
+                    }
+                }
+
+                //Save compressed image
+                sourceImage.Save(savePath, jpegCodec, codecParameter);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
+    
